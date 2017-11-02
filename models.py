4e4823760b7cf6194b96585101b80c0c44d1170e
HagopB <hagop.boghazdeklian@gmail.com>
2017-11-02 17:48:01 +0100

    Add files via upload

--- models.py
+++ models.py
@@ -20,15 +20,7 @@ from scipy.misc import *
 *****************************************************************************
 '''
 def cycle_loss(y_true, y_pred):
-    if k.image_data_format() is 'channels_first':
-        x_w = 2
-        x_h = 3
-    else:
-        x_w = 1
-        x_h = 2
-    loss = k.abs(y_true - y_pred)
-    loss = k.sum(loss, axis=x_h)
-    loss = k.sum(loss, axis=x_w)
+    loss = k.sum(k.abs(y_true - y_pred),axis=-1)
     return loss
 '''
 *****************************************************************************
@@ -87,15 +79,15 @@ class CycleGAN(BaseModel):
                      [dis_fake_B,   dis_fake_A,     rec_A,      rec_B,      fake_B,     fake_A])
 
             G_trainner.compile(Adam(lr=opt.lr, beta_1=opt.beta1,),
-                loss=['MSE',        'MSE',          cycle_loss,      cycle_loss,      cycle_loss,      cycle_loss],
-                loss_weights=[1,    1,              opt.lmbd,   opt.lmbd,   opt.idloss  ,opt.idloss])
+                loss=['MSE', 'MSE', cycle_loss, cycle_loss, cycle_loss, cycle_loss],
+                loss_weights=[1, 1, opt.lmbd, opt.lmbd, opt.idloss, opt.idloss])
         else:
             G_trainner = Model([real_A, real_B],
-                     [dis_fake_B,   dis_fake_A,     rec_A,      rec_B,      ])
+                     [dis_fake_B, dis_fake_A, rec_A, rec_B,])
 
             G_trainner.compile(Adam(lr=opt.lr, beta_1=opt.beta1,),
-                loss=['MSE',        'MSE',          cycle_loss,      cycle_loss,      ],
-                loss_weights=[1,    1,              opt.lmbd,   opt.lmbd,   ])
+                loss=['MSE', 'MSE', cycle_loss, cycle_loss,],
+                loss_weights=[1, 1, opt.lmbd, opt.lmbd,])
         # label:  0             0               real_A      real_B
 
 
@@ -133,88 +125,93 @@ class CycleGAN(BaseModel):
         fake_A_pool = []
         fake_B_pool = []
 
-        iteration = 0
-        while iteration < opt.niter:
-            print('iteration: {}'.format(iteration))
-            # sample
-            real_A = img_A_generator(bs)
-            real_B = img_B_generator(bs)
+        epoch = 0
+        total_iter = 0
+        while epoch < opt.niter:
+            print('Start of epoch: {}'.format(epoch))
+            print('Total number of iterations')
+            max_iter = max([img_A_generator.n_images_, img_B_generator.n_images_])
+            
+            iteration = 0
+            while iteration < max_iter:
+                # sample
+                real_A = img_A_generator(bs)
+                real_B = img_B_generator(bs)
 
-            # fake pool
-            fake_A_pool.extend(self.BtoA.predict(real_B))
-            fake_B_pool.extend(self.AtoB.predict(real_A))
-            fake_A_pool = fake_A_pool[-opt.pool_size:]
-            fake_B_pool = fake_B_pool[-opt.pool_size:]
+                # fake pool
+                fake_A_pool.extend(self.BtoA.predict(real_B))
+                fake_B_pool.extend(self.AtoB.predict(real_A))
+                fake_A_pool = fake_A_pool[-opt.pool_size:]
+                fake_B_pool = fake_B_pool[-opt.pool_size:]
 
-            fake_A = [fake_A_pool[ind] for ind in np.random.choice(len(fake_A_pool), size=(bs,), replace=False)]
-            fake_B = [fake_B_pool[ind] for ind in np.random.choice(len(fake_B_pool), size=(bs,), replace=False)]
-            fake_A = np.array(fake_A)
-            fake_B = np.array(fake_B)
+                fake_A = [fake_A_pool[ind] for ind in np.random.choice(len(fake_A_pool), size=(bs,), replace=False)]
+                fake_B = [fake_B_pool[ind] for ind in np.random.choice(len(fake_B_pool), size=(bs,), replace=False)]
+                fake_A = np.array(fake_A)
+                fake_B = np.array(fake_B)
 
-            ones  = np.ones((bs,)+self.G_trainner.output_shape[0][1:])
-            zeros = np.zeros((bs, )+self.G_trainner.output_shape[0][1:])
-
-
-            # train
-            for _ in range(opt.d_iter):
-                _, D_loss_real_A, D_loss_fake_A, D_loss_real_B, D_loss_fake_B = \
-                    self.D_trainner.train_on_batch([real_A, fake_A, real_B, fake_B],
-                        [zeros, ones*0.9, zeros, ones*0.9])
-
-
-            if opt.idloss > 0:
-                _, G_loss_fake_B, G_loss_fake_A, G_loss_rec_A, G_loss_rec_B, G_loss_id_A, G_loss_id_B = \
-                    self.G_trainner.train_on_batch([real_A, real_B],
-                        [zeros, zeros, real_A, real_B, real_A, real_B])
-            else:
-                _, G_loss_fake_B, G_loss_fake_A, G_loss_rec_A, G_loss_rec_B = \
-                    self.G_trainner.train_on_batch([real_A, real_B],
-                        [zeros, zeros, real_A, real_B, ])
+                ones  = np.ones((bs,)+self.G_trainner.output_shape[0][1:])
+                zeros = np.zeros((bs,)+self.G_trainner.output_shape[0][1:])
 
 
-            print('Generator Loss:')
-            print('fake_B: {} rec_A: {} | fake_A: {} rec_B: {}'.\
-                    format(G_loss_fake_B, G_loss_rec_A, G_loss_fake_A, G_loss_rec_B))
-            if opt.idloss > 0:
-                print('id_loss_A: {}, id_loss_B: {}'.format(G_loss_id_A, G_loss_id_B))
-
-            print('Discriminator Loss:')
-            print('real_A: {} fake_A: {} | real_B: {} fake_B: {}'.\
-                    format(D_loss_real_A, D_loss_fake_A, D_loss_real_B, D_loss_fake_B))
+                # train
+                for _ in range(opt.d_iter):
+                    _, D_loss_real_A, D_loss_fake_A, D_loss_real_B, D_loss_fake_B = \
+                        self.D_trainner.train_on_batch([real_A, fake_A, real_B, fake_B],
+                            [zeros, ones * 0.9, zeros, ones * 0.9])
 
 
-            print("Dis_A")
-            res = self.DisA.predict(real_A)
-            print("real_A: {}".format(res.mean()))
-            res = self.DisA.predict(fake_A)
-            print("fake_A: {}".format(res.mean()))
+                if opt.idloss > 0:
+                    _, G_loss_fake_B, G_loss_fake_A, G_loss_rec_A, G_loss_rec_B, G_loss_id_A, G_loss_id_B = \
+                        self.G_trainner.train_on_batch([real_A, real_B],
+                            [zeros, zeros, real_A, real_B, real_A, real_B])
+                else:
+                    _, G_loss_fake_B, G_loss_fake_A, G_loss_rec_A, G_loss_rec_B = \
+                        self.G_trainner.train_on_batch([real_A, real_B],
+                            [zeros, zeros, real_A, real_B, ])
+                
+                if iteration % 100 == 0:
+                    print('Epoch {} - iteration {}'.format(epoch, iteration))
+                    print('Generator Loss:')
+                    print('fake_B: {} rec_A: {} | fake_A: {} rec_B: {}'.\
+                            format(G_loss_fake_B, G_loss_rec_A, G_loss_fake_A, G_loss_rec_B))
+                    if opt.idloss > 0:
+                        print('id_loss_A: {}, id_loss_B: {}'.format(G_loss_id_A, G_loss_id_B))
 
-            if iteration % opt.save_iter == 0:
-                imga = real_A
-                imga2b = self.AtoB.predict(imga)
-                imga2b2a = self.BtoA.predict(imga2b)
+                    print('Discriminator Loss:')
+                    print('real_A: {} fake_A: {} | real_B: {} fake_B: {}'.\
+                            format(D_loss_real_A, D_loss_fake_A, D_loss_real_B, D_loss_fake_B))
 
-                imgb = real_B
-                imgb2a = self.BtoA.predict(imgb)
-                imgb2a2b = self.AtoB.predict(imgb2a)
 
-                vis_grid(np.concatenate([imga, imga2b, imga2b2a, imgb, imgb2a, imgb2a2b],
-                                                                            axis=0),
-                        6, bs, os.path.join(opt.pic_dir, '{}.png'.format(iteration)) )
+                    print("Dis_A")
+                    res = self.DisA.predict(real_A)
+                    print("real_A: {}".format(res.mean()))
+                    res = self.DisA.predict(fake_A)
+                    print("fake_A: {}".format(res.mean()))
 
-                self.AtoB.save(os.path.join(opt.pic_dir, 'a2b.h5'))
-                self.BtoA.save(os.path.join(opt.pic_dir, 'b2a.h5'))
+                if total_iter % opt.save_iter == 0:
+                    imga = real_A
+                    imga2b = self.AtoB.predict(imga)
+                    imga2b2a = self.BtoA.predict(imga2b)
 
-#               import ipdb
-#               ipdb.set_trace()
-            iteration += 1
-            sys.stdout.flush()
+                    imgb = real_B
+                    imgb2a = self.BtoA.predict(imgb)
+                    imgb2a2b = self.AtoB.predict(imgb2a)
+
+                    vis_grid(np.concatenate([imga, imga2b, imga2b2a, imgb, imgb2a, imgb2a2b],
+                                                                                axis=0),
+                            6, bs, os.path.join(opt.pic_dir, '{}.png'.format(total_iter)) )
+
+                    self.AtoB.save(os.path.join(opt.pic_dir, 'a2b.h5'))
+                    self.BtoA.save(os.path.join(opt.pic_dir, 'b2a.h5'))
+                iteration += 1
+                total_iter += 1
+                sys.stdout.flush()
+            epoch += 1
     def predict(self, path_images, model_path):
         opt = self.opt
- 
         if not os.path.exists(opt.pic_dir):
             os.mkdir(opt.pic_dir)
-			
+            
         img_generator = ImageGenerator(path_images,resize=opt.resize,crop=opt.crop)
         print('Predicting with model' + model_path.split('.')[0])
         
@@ -227,14 +224,13 @@ class CycleGAN(BaseModel):
                                       real[10:15],preds[10:15],real[15:],preds[15:]],axis=0),
                        8, 5,os.path.join(opt.pic_dir, 'demo.png'))       
         
-        
         print('Predicting for all images')
         
         test_data_path = path_images
         images = os.listdir(test_data_path)
         total = len(images)
 
-        imgs = np.ndarray((total, 256, 256, 3), dtype=np.uint8)
+        imgs = np.ndarray((total, opt.shapeA[0], opt.shapeA[1], opt.shapeA[2]), dtype=np.uint8)
         imgs_id = np.ndarray((total, ), dtype=np.int32)
         
         print('Creating test images')
@@ -242,7 +238,7 @@ class CycleGAN(BaseModel):
             img_id = int(image_name.split('.')[0])
 
             img = imread(os.path.join(test_data_path, image_name))
-            img =  imresize(img,(256, 256))
+            img =  imresize(img, opt.crop)
 
             imgs[idx] = img
             imgs_id[idx] = img_id
